9d47316b0ebb869612a52741959956d0f04ac160
unknown <paul.virkus@gmail.com>
2024-10-12 20:47:10 +0300

    added raytrace to camera and debug dot

--- scenes/camera_raycast.gd
+++ scenes/camera_raycast.gd
@@ -1,13 +1,20 @@
 extends Camera3D
 
 var rayRange := 2000
+#const hitMarker = preload("hitLocMarker.tscn")
+var hitMarker: PackedScene = preload("res://scenes/hitLocMarker.tscn")
 
-func _input(event):
-	if event.is_action_pressed("shoot"):
-		getCameraCollision()
+#func _input(event):
+	#if event.is_action_pressed("shoot"):
+		#getCameraCollision()
 	
+func spawnHitMarker(position: Vector3, parent):
+	var hitMarkerInst = hitMarker.instantiate()  # Create an instance of the marker scene
+	hitMarkerInst.global_transform.origin = position  # Set the position of the marker
+	var world = get_tree().get_root()
+	world.add_child(hitMarkerInst)  # Add the marker to the scene tree
 	
-func getCameraCollision():
+func getCameraCollision(rangeInt, dmgInt, offsetRad):
 	var center = get_viewport().get_size()/2
 	
 	var rayOrigin = project_ray_origin(center)
@@ -15,11 +22,19 @@ func getCameraCollision():
 	
 	var newIntersection = PhysicsRayQueryParameters3D.create(rayOrigin, rayEnd)
 	var intersection = get_world_3d().direct_space_state.intersect_ray(newIntersection)
-
+	
 	if not intersection.is_empty():
-		print(intersection.collider.name)
+		var hitObject = intersection.collider
+		spawnHitMarker(intersection.position, hitObject)
+		if hitObject.has_method("takeDmg"):
+			hitObject.takeDmg(hitObject)
+		
+		print(hitObject.name)
 	else:
 		print("nothing")
+	
+	
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
