a27a1fe2b77cdfabd14b27311be238cf6c9cc845
Promise Temitope <oluwatobilobatemi05@gmail.com>
2024-10-12 13:24:53 +0300

    initialized character3D with movement

---
+++ scripts/movement.gd
@@ -0,0 +1,34 @@
+extends Node3D
+
+@export var speed := 5
+@export var jump_speed := 5
+var velocity : Vector3 = Vector3()
+var player : CharacterBody3D 
+var camera: Camera3D
+
+func _ready():
+	player = get_parent() as CharacterBody3D
+	camera = get_node("../TwistPivot/PitchPivot/CameraController/Camera3D")
+
+func apply_movement(input_vector: Vector2):
+	if input_vector.length() > 0:
+		# Get the forward and right direction from the camera
+		var movement_dir = camera.global_transform.basis * Vector3(input_vector.x, 0, input_vector.y).normalized()
+		velocity.x = movement_dir.x * speed
+		velocity.z = movement_dir.z * speed
+	else:
+		velocity.x = 0  # Stop movement if no input
+		velocity.z = 0
+
+
+func apply_gravity(gravity: float, delta: float):
+	if not player.is_on_floor():
+		velocity.y -= gravity * delta
+
+func jump():
+	if player.is_on_floor():
+		velocity.y = jump_speed
+
+func update_movement():
+	player.velocity = velocity  # Set player velocity
+	player.move_and_slide()  # No arguments needed
