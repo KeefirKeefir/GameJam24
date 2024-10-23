534e41e86f10b8c478a174b4e61020e7bbb49ae0
keefir <paul.virkus@gmail.com>
2024-10-24 02:52:05 +0300

    hacking system is now functional, needs to be expanded

--- Player/hackHandler.gd
+++ Player/hackHandler.gd
@@ -63,7 +63,12 @@ func detectEnemies() -> void:
 		HTargetName.text = str(target.name)
 	else: 
 		HTargetName.text = ""
-	
+		
+	if allTargets.is_empty():
+		target = null
+		for label in HHacks.get_children():
+			HHacks.remove_child(label)
+			label.queue_free()
 
 func hackMenu() -> void:
 	for label in HHacks.get_children():
@@ -76,15 +81,19 @@ func hackMenu() -> void:
 		PanelInst.myhack = dataHack
 		PanelInst.text = h.getName(dataHack)
 	
-	if HHacks.get_child_count() > 0:
+	if focused_index > HHacks.get_child_count() - 1:
+		focused_index = 0
+	elif HHacks.get_child_count() > 0:
 		HHacks.get_child(focused_index).highlight()
+	#if target == null:
+		#for label in HHacks.get_children():
+			#HHacks.remove_child(label)
+			#label.queue_free()
 
 func _process(delta: float) -> void:
 	detectEnemies()
-	if target != lastTarget:
-		if target and hacking:
-			hackMenu()
-	target = lastTarget
+	if target and hacking:
+		hackMenu()
 	if hacking == true:
 		Engine.time_scale = move_toward(Engine.time_scale, 0.3, 0.02)
 	else:
@@ -122,7 +131,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		hacking = true
 
 	elif event.is_action_released("hack"):
-		if HHacks.get_child_count() > 0 and target:
+		if HHacks.get_child_count() > 0:
 			target.recvHack(HHacks.get_child(focused_index).myhack) 
 		hacking = false
 		focused_index = 0
